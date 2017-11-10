42e79709ea78a8f1e290d7ab6fb9d757dba543f7
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-09 22:06:49 -0200

    Adiciona routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import App from './app/App'
+import Home from './pages/home/Home';
+import Login from './pages/auth/login/Login';
 
-render(<App />, document.getElementById('app'));
+render((
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/login' component={Login}/>
+    </Switch>
+   </BrowserRouter>
+), document.getElementById('app'));
---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Menu from './components/Menu';
+
+import '../../resources/css/style.css';
+
+export default class Home extends Component {
+  render() {
+    return (<Menu></Menu>);
+  }
+}
